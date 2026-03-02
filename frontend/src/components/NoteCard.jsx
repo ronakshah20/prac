14bab86e47f724c0dc8a3d0f9e60012c9e23f837
ronakshah22-lab <ronakshah22@gnu.ac.in>
2026-03-03 01:00:47 +0530

    UI Changes

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -21,7 +21,7 @@ const NoteCard = ({note, setNotes}) => {
         }
     }
     return (
-        <Link to={`/note/${note._id}`} className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9d]'>
+        <Link to={`/note/${note._id}`} className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#ea00ff]'>
             <div className='card-body'>
                 <h3 className='card-title text-base-content'>{note.title}</h3>
                 <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
